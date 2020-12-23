2a5a7ec4e120a57c4ff8be2fe5a58ecfbc4e2190
ymoth <69197682+ymoth@users.noreply.github.com>
2020-12-23 20:57:41 +0600

    калькулятор пидорас

--- src/commands/calc.py
+++ src/commands/calc.py
@@ -3,42 +3,24 @@ from vkbottle.rule import FromMe
 from unit import edit_msg
 user = Blueprint("calculator")
 from prefixs import p, sticker
-@user.on.chat_message(FromMe(),text=[p+'реши <cul1> + <cul2>', p+'реши <cul1>+<cul2>'],lower=True)
+@user.on.message_handler(FromMe(),text=[p+'реши <cul1> + <cul2>', p+'реши <cul1>+<cul2>'],lower=True)
 async def messages(ans: Message, cul1: int, cul2: int):
-    await ans(f'{sticker}Решил быстро, ваш ответ:\n {int(cul1) + int(cul2)}.', reply_to=ans.conversation_message_id)
+    await ans(f'{sticker}Решил быстро, ваш ответ:\n {int(cul1) + int(cul2)}.')
 
 
-@user.on.chat_message(FromMe(),text=[p+'реши <cul1> - <cul2>', p+'реши <cul1>-<cul2>'],lower=True)
+@user.on.message_handler(FromMe(),text=[p+'реши <cul1> - <cul2>', p+'реши <cul1>-<cul2>'],lower=True)
 async def messages(ans: Message, cul1: int, cul2: int):
-    await ans(f'{sticker}Решил быстро, ваш ответ:\n {int(cul1) - int(cul2)}.',reply_to=ans.conversation_message_id)
+    await ans(f'{sticker}Решил быстро, ваш ответ:\n {int(cul1) - int(cul2)}.')
 
 
-@user.on.chat_message(FromMe(),text=[p+'реши <cul1> * <cul2>',p+"реши <cul1>*<cul2>"],lower=True)
+@user.on.message_handler(FromMe(),text=[p+'реши <cul1> * <cul2>',p+"реши <cul1>*<cul2>"],lower=True)
 async def messages(ans: Message, cul1: int, cul2: int):
     await ans(f'{sticker}Решил быстро, ваш ответ:\n {int(cul1) * int(cul2)}.',reply_to=ans.conversation_message_id)
 
-@user.on.chat_message(FromMe(),text=[p+'реши <cul1> / <cul2>',p+"реши <cul1>*<cul2>"],lower=True)
+@user.on.message_handler(FromMe(),text=[p+'реши <cul1> / <cul2>',p+"реши <cul1>/<cul2>"],lower=True)
 async def messages(ans: Message, cul1: int, cul2: int):
-    await ans(f'{sticker}Решил быстро, ваш ответ:\n {int(cul1) / int(cul2)}.',reply_to=ans.conversation_message_id)
+    await ans(f'{sticker}Решил быстро, ваш ответ:\n {int(cul1) / int(cul2)}.')
 
-
-@user.on.message(FromMe(),text=[p+'реши <cul1> + <cul2>', p+'реши <cul1>+<cul2>'],lower=True)
-async def messages(ans: Message, cul1: int, cul2: int):
-    await ans(f'{sticker}Решил быстро, ваш ответ:\n {int(cul1) + int(cul2)}.', reply_to=ans.message_id)
-
-
-@user.on.message(FromMe(),text=[p+'реши <cul1> - <cul2>', p+'реши <cul1>-<cul2>'],lower=True)
-async def messages(ans: Message, cul1: int, cul2: int):
-    await ans(f'{sticker}Решил быстро, ваш ответ:\n {int(cul1) - int(cul2)}.',reply_to=ans.message_id)
-
-
-@user.on.message(FromMe(),text=[p+'реши <cul1> * <cul2>',p+"реши <cul1>*<cul2>"],lower=True)
-async def messages(ans: Message, cul1: int, cul2: int):
-    await ans(f'{sticker}Решил быстро, ваш ответ:\n {int(cul1) * int(cul2)}.',reply_to=ans.message_id)
-
-@user.on.message(FromMe(),text=[p+'реши <cul1> * <cul2>',p+"реши <cul1>*<cul2>"],lower=True)
-async def messages(ans: Message, cul1: int, cul2: int):
-    await ans(f'{sticker}Решил быстро, ваш ответ:\n {int(cul1) / int(cul2)}.',reply_to=ans.message_id)
 
 
 
