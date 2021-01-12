281db9fc2eaddd6c566964abec3916501b90eb92
yMoth <tophanbig@gmail.com>
2021-01-12 23:06:16 +0600

    ну как бы
    я ток добавил рандом от нехер делать, юху
    {префикс} рчисло (от до)
    {префикс} рчисло 1 20

--- src/commands/random.py
+++ src/commands/random.py
@@ -1,31 +1,34 @@
 from vkbottle.rule import FromMe
-from vkbottle.user import Blueprint,Message
+from vkbottle.user import Blueprint, Message
 from prefixs import p
 import random
 from unit import edit_msg
 from loguru import logger
+
 bp = Blueprint("RANDOM")
 
 
-@bp.on.message_handler(FromMe(),text=[p+'рандом <cul1> или <cul2>',p+"выбери <cul1> или <cul2>"],lower=True)
-async def Leand(ans:Message, cul1:str,cul2:str):
+@bp.on.message_handler(FromMe(), text=[p + 'рандом <cul1> или <cul2>', p + "выбери <cul1> или <cul2>"], lower=True)
+async def Leand(ans: Message, cul1: str, cul2: str):
     spisok = [cul1, cul2]
-    await ans(f'Хм, я выбираю: {spisok[random.randint(0,1)]}',reply_to=ans.id)
+    await ans(f'Хм, я выбираю: {spisok[random.randint(0, 1)]}', reply_to=ans.id)
 
 
 @bp.on.message_handler(text=[
-        p+"вероятность того что <thing",
-        p+"вероятность что <thing",
-        p+"вероятность <thing>"
-        ],lower=True)
+    p + "вероятность того что <thing",
+    p + "вероятность что <thing",
+    p + "вероятность <thing>"
+], lower=True)
 async def probability(ans: Message, thing: str):
     await edit_msg(
         ans, f"Вероятность того, что {thing} равна {round(random.uniform(0.0, 1.0) * 100, 2)}%"
     )
+
+
 @logger.catch()
-@bp.on.chat_message(FromMe(),text=p+'кто <cul1>',lower=True)
-async def RandomChat(ans:Message, cul1:str):
-    chat = await bp.api.request("messages.getChat", {"chat_id":ans.chat_id})
+@bp.on.chat_message(FromMe(), text=p + 'кто <cul1>', lower=True)
+async def RandomChat(ans: Message, cul1: str):
+    chat = await bp.api.request("messages.getChat", {"chat_id": ans.chat_id})
     userschat = chat['users']
     randomid = random.choice(userschat)
     user = await bp.api.users.get(user_ids=randomid)
@@ -33,4 +36,12 @@ async def RandomChat(ans:Message, cul1:str):
 Ух-ты, {cul1} это у нас 
 ❗ | @id{user[0].id}({user[0].first_name} {user[0].last_name})
 """
-    await edit_msg(ans,msg)
+    await edit_msg(ans, msg)
+
+
+@bp.on.message_handler(FromMe(), text=p + "рчисло <cul1> <cul2>", lower=True)
+async def RandomSize(ans: Message, cul1, cul2):
+    from unit import randomizer
+    from prefixs import error_sticker,sticker
+    result = randomizer(a_=int(cul1), b_=int(cul2))
+    await edit_msg(ans,f"{sticker}Ваше рандомное число: {result}")
