4e8e4dbfbb46788f0ef020b4bb5de56fb44cc728
yMoth <tophanbig@gmail.com>
2021-01-18 19:40:37 +0600

    Fix for Termux.
    Description -
    full delete documentation  "pillow" as "PIL", and to create new command, {prefix} "ксмс"

--- run.py
+++ run.py
@@ -29,13 +29,13 @@ user = User(data['token'], mobile=True)
 
 from src.commands import iris,id,signals,time, like, calc,wiki,msgdel,addfriends,commentadd,info,random,online,commands,shubs
 import unit
-import quote
+
 from requests import get as rget
 from unit import __version__, __author__, __namelp__
 user.set_blueprints(time.bp,unit.bp,like.bp,
 calc.user,wiki.bp,msgdel.bp,
 addfriends.bp,info.bp,commentadd.bp,
-random.bp,online.bp,commands.bp,shubs.bp,signals.bp, id.bp,iris.bp,quote.bp,quote.bp)
+random.bp,online.bp,commands.bp,shubs.bp,signals.bp, id.bp,iris.bp)
 user_id = (rget(f'https://api.vk.com/method/users.get?&v=5.52&access_token={token}').json())['response'][0]['id']
 async def start():
     from unit import __author__, __version__, __namelp__
